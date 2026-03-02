f05f5ed26c5640a1b584036e0c91f1371845d9f4
Moisés <149697725+swallowsoul@users.noreply.github.com>
2026-03-02 00:03:46 -0300

    Update ValorWorgen.lua

--- ValorWorgen.lua
+++ ValorWorgen.lua
@@ -1,25 +1,27 @@
--------------------------------------------------
--- ValorWorgen
--- Global: ValorAddons[] ValorWorgenForm
--------------------------------------------------
 local _G = _G
 local addonName = ...
-if not select(2, _G.UnitRace("player")) == "Worgen" then return end
-if not _G.ValorAddons then _G.ValorAddons = {} end
-_G.ValorAddons[addonName] = true
-_G.ValorWorgenForm = false
 
--- Local Variables
--------------------------------------------------
+local playerRace = select(2, _G.UnitRace("player"))
+if not (playerRace == "Worgen" or playerRace == "Dracthyr") then return end
+if not _G.RevertForm then RevertForm = {} end
+_G.RevertForm[addonName] = true
+
 local playerModel = _G.CreateFrame("PlayerModel")
 local macroCond = "[nocombat,nomounted,novehicleui]"
-local macroText = "/cast "..macroCond.." ".._G.GetSpellInfo(68996).."\n/run ClearOverrideBindings(ValorWorgenButton)"
-local modelId = { [307454] = "w", [307453] = "w", }		-- Male/Female Worgen
+local spellId, modelId
+
+if playerRace == "Worgen" then
+	spellId = 68996
+	modelId = {[307454] = "w", [307453] = "w"}	-- Worgen
+elseif playerRace == "Dracthyr" then
+	spellId = 372014
+	modelId = {[4207724] = "d"}					-- Dracthyr
+end
+
+local macroText = "/cast " .. macroCond .. " " .. _G.C_Spell.GetSpellName(spellId) .. "\n/run ClearOverrideBindings(RevertFormButton)"
 local GetBindingKey, ClearOverrideBindings, SetOverrideBindingClick, InCombatLockdown, SecureCmdOptionParse
 	= _G.GetBindingKey, _G.ClearOverrideBindings, _G.SetOverrideBindingClick, _G.InCombatLockdown, _G.SecureCmdOptionParse
 
--- setupKeyBinding( frame )
--------------------------------------------------
 local function setupKeyBinding(f)
 	ClearOverrideBindings(f)
 	local k1, k2 = GetBindingKey("TOGGLESHEATH")
@@ -27,38 +29,36 @@ local function setupKeyBinding(f)
 	if k2 then SetOverrideBindingClick(f, false, k2, f:GetName()) end
 end
 
--- Where the magic happens...
--------------------------------------------------
-local vwButton = _G.CreateFrame("Button", "ValorWorgenButton", nil, "SecureActionButtonTemplate")
-vwButton:SetAttribute("type", "macro")
-vwButton:RegisterEvent("UNIT_MODEL_CHANGED")
-vwButton:RegisterEvent("UNIT_AURA")
-vwButton:RegisterEvent("PLAYER_REGEN_ENABLED")
-vwButton:RegisterEvent("PLAYER_ENTERING_WORLD")
-vwButton:RegisterForClicks("LeftButtonDown", "LeftButtonUp")
-vwButton:SetScript("OnEvent",
+local revertFormButton = _G.CreateFrame("Button", "RevertFormButton", nil, "SecureActionButtonTemplate")
+revertFormButton:SetAttribute("type", "macro")
+revertFormButton:RegisterEvent("UNIT_MODEL_CHANGED")
+revertFormButton:RegisterEvent("UNIT_AURA")
+revertFormButton:RegisterEvent("PLAYER_REGEN_ENABLED")
+revertFormButton:RegisterEvent("PLAYER_ENTERING_WORLD")
+revertFormButton:RegisterForClicks("LeftButtonDown", "LeftButtonUp")
+revertFormButton:SetScript("OnEvent",
 	function(self, event, ...)
 		-- Model Trigger: Determine Current Form
 		if event == "UNIT_MODEL_CHANGED" and ... == "player" then
 			playerModel:SetUnit("player")
 			local m = playerModel:GetModelFileID()
 			if m then
-				_G.ValorWorgenForm = modelId[m] and true or false
+				_G.RevertForm = modelId[m] and true or false
 				if not InCombatLockdown() then
-					if _G.ValorWorgenForm and SecureCmdOptionParse(macroCond) then
-						setupKeyBinding(self)			-- Is Worgen, Can Transform; Set KeyBind
+					if _G.RevertForm and SecureCmdOptionParse(macroCond) then
+						setupKeyBinding(self)			-- Is Transformable, Can Transform; Set KeyBind
 						self:SetAttribute("macrotext", macroText)
 					else
-						ClearOverrideBindings(self)		-- Not Worgen or Can't Transform; Unset
+						ClearOverrideBindings(self)		-- Not Transformable or Can't Transform; Unset
 						self:SetAttribute("macrotext", "")
 					end
 				end
 			end
-		elseif not InCombatLockdown() and _G.ValorWorgenForm and SecureCmdOptionParse(macroCond) then
-			setupKeyBinding(self)			-- Event Trigger: Is Worgen, Can Transform; Set KeyBind
+		elseif not InCombatLockdown() and _G.RevertForm and SecureCmdOptionParse(macroCond) then
+			setupKeyBinding(self)			-- Event Trigger: Is Transformable, Can Transform; Set KeyBind
 			self:SetAttribute("macrotext", macroText)
 		elseif not InCombatLockdown() then
-			ClearOverrideBindings(self)		-- Event Trigger: Not Worgen or Can't Transform; Unset
+			ClearOverrideBindings(self)		-- Event Trigger: Not Transformable or Can't Transform; Unset
 			self:SetAttribute("macrotext", "")
 		end
 	end
